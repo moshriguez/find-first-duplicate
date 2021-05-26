92a2c6e0264b4eaf5f7c9e94bf5a16f797ff524c
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-26 11:45:58 -0700

    completed both Ruby and JS

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,16 +1,32 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+	// type your code here
+	const newArr = [];
+  for (let element of arr) {
+		if (newArr.includes(element)) {
+      return element;
+    } else {
+      newArr.push(element);
+    }
+	};
+  return -1;
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+	// add your own tests in here
+	console.log('Expecting: 3');
+	console.log('=>', findFirstDuplicate([2, 1, 3, 3, 2]));
 
-  console.log("");
+	console.log('Expecting: -1');
+	console.log('=>', findFirstDuplicate([1, 2, 3, 4]));
 
-  console.log("Expecting: -1");
-  console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+	console.log('Expecting: 5');
+	console.log('=>', findFirstDuplicate([5, 5, 1, 2, 3, 4]));
+
+	console.log('Expecting: 1');
+	console.log('=>', findFirstDuplicate([1, 2, 3, 4, 1]));
+
+	console.log('Expecting: 5');
+	console.log('=>', findFirstDuplicate([1, 2, 3, 4, 5, 5]));
 }
 
 module.exports = findFirstDuplicate;
--- ruby/find_first_duplicate.rb
+++ ruby/find_first_duplicate.rb
@@ -1,16 +1,44 @@
 def find_first_duplicate(arr)
   # type your code in here
+  # FIRST ATTEMPT -- does not work on 1st test; all others pass
+  # if arr.length == 1
+  #   return -1
+  # end
+  # runner = arr[0]
+  # arr_temp = arr.drop(1)
+  # arr_temp.each do |num|
+  #   if runner == num
+  #     return runner
+  #   end
+  # end
+  # find_first_duplicate(arr_temp)
+
+  new_arr = []
+  arr.each do |num|
+    if new_arr.include?(num)
+      return num
+    else
+    new_arr << num
+    end
+  end
+  -1
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 3"
   puts "=>", find_first_duplicate([2, 1, 3, 3, 2])
 
-  puts
-
   puts "Expecting: -1"
   puts "=>", find_first_duplicate([1, 2, 3, 4])
-
+  
+  puts "Expecting: 5"
+  puts "=>", find_first_duplicate([5, 5, 1, 2, 3, 4])
+  
+  puts "Expecting: 1"
+  puts "=>", find_first_duplicate([1, 2, 3, 4, 1])
+  
+  puts "Expecting: 5"
+  puts "=>", find_first_duplicate([1, 2, 3, 4, 5, 5])
   # Don't forget to add your own!
 end
 
